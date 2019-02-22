348ab5ee279fa0f31823b0834965fbd07ba38512
Lily <lgburtness@gmail.com>
2019-02-21 22:25:51 -0500

    Draw simple bar chart with D3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './BarChart'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Lily Burtness
           </p>
-          <a
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
 
-        <barChart/>
+        <body className="App-body">
+          <BarChart data={[5,10,1,3]} size={[500,500]}/>
+        </body>
       </div>
     );
   }
